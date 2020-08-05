cbc92191e006de005c56632ec753a4596683ab44
Jntmkk <bbmike1086@gmail.com>
2020-08-05 13:30:23 +0800

    修改 OneNetServiceImpl

--- src/main/java/xyz/example/demo/service/impl/OneNetServiceImpl.java
+++ src/main/java/xyz/example/demo/service/impl/OneNetServiceImpl.java
@@ -1,8 +1,9 @@
 package xyz.example.demo.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Service;
 import xyz.example.demo.service.OneNetService;
-
+@Service
 public class OneNetServiceImpl implements OneNetService {
     @Override
     public JSONObject sendCommand(String deviceId, String cmd) {
