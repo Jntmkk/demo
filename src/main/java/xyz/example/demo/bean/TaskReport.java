4a71b155a8547f2443b0822e734f9175059aab54
Jntmkk <bbmike1086@gmail.com>
2020-08-09 16:35:43 +0800

    test getpostedTask

--- src/main/java/xyz/example/demo/bean/TaskReport.java
+++ src/main/java/xyz/example/demo/bean/TaskReport.java
@@ -2,9 +2,11 @@ package xyz.example.demo.bean;
 
 import lombok.Data;
 
+import java.math.BigInteger;
+
 @Data
 public class TaskReport {
-    Long belongTo;
+    BigInteger belongToTask;
     String solution;
     String pointer;
 }
